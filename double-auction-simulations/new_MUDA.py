f9b51cea40319f66c4601bfe57c62d0b1435129c
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-08-19 01:00:50 +0300

    experimental stuff

--- double-auction-simulations/new_MUDA.py
+++ double-auction-simulations/new_MUDA.py
@@ -2,6 +2,8 @@
 
 """
 Experiments with a new multi-unit double-auction mechanism.
+
+EXPERIMENTAL STUFF
 """
 
 from random_datasets import randomValuations, randomAuction
@@ -24,12 +26,13 @@ def newMUDA(buyers:list, numOfItems:int):
 
     print("mapIndexToQuantity",mapIndexToQuantity)
     buyers_sorted_by_2 = sorted(buyers, key=lambda t:t.valueOf(numBundles=2), reverse=True)
-    print("buyers_sorted_by_2",buyers_sorted_by_2)
+    print("buyers_sorted_by_2")
+    for b in buyers_sorted_by_2: print("  ",b)
 
     # calculate price for 2 units:
     price_2 = 0
     for b in buyers_sorted_by_2:
-        if mapIndexToQuantity.get(b.index,0)==0:
+        if mapIndexToQuantity.get(b.index,0)!=2: #==0:#
             price_2 = b.valueOf(numBundles=2)
             break
     print("price_2", price_2)
@@ -37,7 +40,7 @@ def newMUDA(buyers:list, numOfItems:int):
     # calculate price for 1 units:
     price_1 = 0
     for b in vbuyers_sorted_by_1:
-        if mapIndexToQuantity.get(b[2],0)==0:
+        if mapIndexToQuantity.get(b[2],0)!=1: #==0: #
             price_1 = b[1]
             break
     print("price_1", price_1)
@@ -61,16 +64,17 @@ def newMUDA(buyers:list, numOfItems:int):
 
 if __name__ == "__main__":
     # seed = 47585  # bad for "<1,<2"
-    # seed = 464410 # bad for "!=1,!=2"
-    # seed = 50193  # bad for ">0,>0"
+    # seed = 115423 # bad for "==0,==0"
+    # seed = 353511 # bad for "!=1,!=2"
     seed = int(np.round(np.random.random()*1000000))
     print("seed",seed)
     np.random.seed(seed)
-    numOfTraders = 6
+    numOfSellers = 4
+    numOfRealBuyers = 6
     traders = []
-    for i in range(numOfTraders):
+    for i in range(numOfRealBuyers):
         traders.append(Trader.Buyer(randomValuations(
             minNumOfUnits=1, maxNumOfUnits=2, meanValue=10, maxNoiseSize=10, round=True, index=i), index=i))
     print("traders",traders)
-    print(newMUDA(traders, numOfItems=5))
+    print(newMUDA(traders, numOfItems=numOfSellers))
 
